04a7d639b6e2623d85f3d5685f9de2649e993c0b
Suvrajyoti Mukhopadhyay <suvivek122004@gmail.com>
2024-09-14 17:16:45 +0530

    F1

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let boxes = document.querySelectorAll("#box");
 
 let head = document.querySelector(".head");
 let h1 = document.querySelector("h1")
-console.dir(head);
+//console.dir(head);
 head.style.display="none";
 let game = document.querySelector(".game");
 const winpat = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
@@ -32,6 +32,7 @@ boxes.forEach((box) => {
 
 const iswin = ()=>{
     count++;
+    console.log(count)
     for(let pat of winpat)
     {
         pos1 = boxes[pat[0]].innerText;
@@ -40,7 +41,7 @@ const iswin = ()=>{
 
         if(pos1 !== "" && pos2 !== "" && pos3 !== "" )
         {
-            if(pos1 == pos2 && pos2 == pos3)
+            if(pos1 === pos2 && pos2 === pos3)
             {
                 for(let b of boxes)
                 {
@@ -52,15 +53,20 @@ const iswin = ()=>{
                 head.style.display="block";
                 h1.innerText = `!!! Congratulation ${pos1} is Winner !!!`;
             }
+            else if(count===9 && pos1 !== pos2 && pos2 !== pos3)
+            {
+                for(let b of boxes)
+                    {
+                        b.disabled = true;
+                    }
+                head.style.marginTop="20%";
+                game.style.marginTop="30%";
+                head.style.display="block";
+                h1.innerText = `!!! DRAW !!!`;
+            }
         }
     }
-    if(count==9)
-    {
-        head.style.marginTop="20%";
-        game.style.marginTop="30%";
-        head.style.display="block";
-        h1.innerText = `!!! DRAW !!!`;
-    }
+    
 }
 
 let reset = document.querySelector("#reset");
@@ -70,6 +76,12 @@ reset.addEventListener("click", ()=>{
             b.innerText="";
             b.disabled = false;
         }
+        count = 0;
+        pos1 = "";
+        pos2 = "";
+        pos3 = "";
+        h1.innerText = ``;
+
 })
 
 
@@ -82,4 +94,10 @@ n.addEventListener("click",()=>{
             head.style.display="none";
             game.style.marginTop="40px";
         }
+        //turnO=true;
+        pos1 = "";
+        pos2 = "";
+        pos3 = "";
+        h1.innerText = ``;
+        count= 0;
 })
